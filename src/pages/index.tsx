4021b0f4c202a05ca465a0a34f1ac2e524b18a45
YanAnderson-bit <yan.anderson135@gmail.com>
2023-09-24 13:18:41 -0300

    Creates navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
+import { Box, Flex, Avatar, Text, HStack, Image } from '@chakra-ui/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${inter.className}`}></main>
+      <main className={`${inter.className}`}>
+        <Box height="100vh">
+          <Flex
+            as="nav"
+            bg="white"
+            width="100%"
+            height="60px"
+            boxShadow="md"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            paddingX="50px"
+            paddingY="35px"
+          >
+            <Image alt="logo" src="/icon.jpg" boxSize="50px" />
+            <HStack spacing={5}>
+              <Text>Yan Anderson</Text>
+              <Avatar
+                fontWeight="bold"
+                color="#52606c"
+                bg="#a49ccc"
+                name="Yan Anderson"
+              />
+            </HStack>
+          </Flex>
+        </Box>
+      </main>
     </>
   );
 }
