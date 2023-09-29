e7bd5c5a414995ad7c9da620582210e580dfbd89
YanAnderson-bit <yan.anderson135@gmail.com>
2023-09-28 23:54:14 -0300

    finishes form add item

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,8 @@
 import Layout from '@/components/Layout';
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import CreateProductForm from './components/CreateProductForm';
 import Header from './components/CreateProductForm/components/Header';
+import CreateItemForm from './components/CreateItemForm';
 
 export default function ProductsPage() {
   return (
@@ -9,6 +10,13 @@ export default function ProductsPage() {
       <Box mt="20px">
         <Header />
         <CreateProductForm />
+        <CreateItemForm />
+        <Flex justifyContent="flex-end" gap={5} mt="3rem">
+          <Button bg="#ebebeb" w="5rem">
+            <Text color="#333333">Cancelar</Text>
+          </Button>
+          <Button>Criar</Button>
+        </Flex>
       </Box>
     </Layout>
   );
