926ba2ab9844bfc132b701c10bfe114ce1dc8fd2
YanAnderson-bit <yan.anderson135@gmail.com>
2023-09-29 15:08:01 -0300

    Creates FormInput and FormInputError component

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,6 +1,19 @@
 import { Box, Input, InputProps } from '@chakra-ui/react';
+import { ValidationRule, useFormContext } from 'react-hook-form';
 
-export default function FormInput(props: InputProps) {
+type FormInputProps = {
+  name: string;
+  isRequired?: boolean;
+  pattern?: ValidationRule<RegExp> | undefined;
+} & InputProps;
+
+export default function FormInput({
+  isRequired = false,
+  pattern = undefined,
+  ...props
+}: FormInputProps) {
+  const { register } = useFormContext();
+  console.log(props.name);
   return (
     <Input
       border="none"
@@ -8,6 +21,7 @@ export default function FormInput(props: InputProps) {
       bg="#F3F5F6"
       type="string"
       {...props}
+      {...register(props.name, { required: isRequired, pattern })}
     />
   );
 }
